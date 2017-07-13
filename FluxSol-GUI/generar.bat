c066439af8b8fa5e2622f6d802891997e90887eb
lucianouba <luciano.buglioni@gmail.com>
2017-07-13 09:48:10 -0300

    Corrected  generar from FluxSol GUI dir.

--- FluxSol-GUI/generar.bat
+++ FluxSol-GUI/generar.bat
@@ -1,4 +1,3 @@
-set FLUXSOL_DIR=D:/Luciano/Numerico/MecSol/FluxSol/Src
 set VTK_DIR=D:/Luciano/Libs/VTK-7.1.1-Install/
 
 set PATH=D:/Luciano/Programas/x86_64-5.3.0-posix-seh-rt_v4-rev0/mingw64/bin;D:/Luciano/Programas/CMake/bin;
@@ -13,6 +12,6 @@ set PETSC_ARCH=arch-mswin-mpi1-release
 set CXX=x86_64-w64-mingw32-g++.exe
 set CC=x86_64-w64-mingw32-gcc.exe
 
-cmake -G "CodeBlocks - MinGW Makefiles" -DCMAKE_CXX_COMPILER=%CXX% -DCMAKE_BUILD_TYPE=DEBUG _DCMAKE_CXX_FLAGS="-g" %FLUXSOL_DIR%/FluxSol-GUI
+cmake -G "CodeBlocks - MinGW Makefiles" -DCMAKE_CXX_COMPILER=%CXX% -DCMAKE_BUILD_TYPE=DEBUG _DCMAKE_CXX_FLAGS="-g" %FLUXSOL_DIR%/Src/FluxSol-GUI
 
 mingw32-make.exe
